43f4dfea3a56a9e130263a0ed1583ab853a4c0c4
Alan Alves <alan.alvess@outlook.com>
2025-09-26 05:30:02 -0300

    Corrigindo Configs

--- professor-service/src/main/java/com/diario/professorservice/ProfessorServiceApplication.java
+++ professor-service/src/main/java/com/diario/professorservice/ProfessorServiceApplication.java
@@ -2,8 +2,10 @@ package com.diario.professorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProfessorServiceApplication {
 
     public static void main(String[] args) {
